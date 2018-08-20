9554c05d4c840834709b9896fc71cc318f624622
hus5522 <hus5522@naver.com>
2018-08-20 20:26:56 +0900

    V1.0
    V1.0

---
+++ Script/FirstGameStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class FirstGameStart : MonoBehaviour {
+
+
+    public void GoToSelectGame()
+    {
+        SceneManager.LoadScene("MainGameScreen");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
